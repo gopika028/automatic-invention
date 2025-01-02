5f091c3569f77b97d3ba15c75fdb6e5168ae3c26
gopika028 <24sbca028@psgrkcw.ac.in>
2025-01-02 08:57:05 +0000

    modified:   notebooks/prg15.py

---
+++ notebooks/prg15.py
@@ -0,0 +1,6 @@
+def add_item(cart):
+    item=input("Enter item name:")
+    price=float(input(f"Enter price of{item}:rs."))
+    quantity=int(input(f"Enter quantity of{item}:rs."))
+    quantity=int(input(f"Enter quantity of{item}:rs."))
+    
